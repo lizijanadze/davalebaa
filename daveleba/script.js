519772b0b905ca9d140b9c1361054f86b8f7a989
lizijanadze <164235851+lizijanadze@users.noreply.github.com>
2024-06-12 18:19:02 +0400

    Add files via upload

---
+++ daveleba/script.js
@@ -0,0 +1,62 @@
+
+
+
+
+let ricxvi = 10
+
+switch (ricxvi){
+    case 10:
+    console.log("es cvladi aris 10")
+    break;
+
+    case 11:
+    console.log("es cvladi aris 11")
+    break;
+
+    case 12:
+    console.log("es cvladi aris 12")
+    break;
+
+    case 13:
+    console.log("es cvladi aris 13")
+    break;
+
+    case 14:
+    console.log("es cvladi aris 14")
+    break;
+
+    default:
+        console.log("es cvladi aris 15")
+}
+
+
+
+
+let saxelebi = ["mariami lizi tekla luka andria"]
+
+saxelebi.shift()
+
+saxelebi.push ("mariami")
+
+if (saxelebi [4] == "mariami") {
+    console.log( "mariami")
+}
+else if (saxelebi [4] == "lizi") {
+    console.log ("lizi")
+}
+
+else if (saxelebi [4] == "tekla") {
+    console.log ("tekla")
+}
+
+else if (saxelebi [4] == "luka") {
+    console.log ("luka")
+}
+
+
+else {
+    console.log ("andria")
+}
+
+
+
